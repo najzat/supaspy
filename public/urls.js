03800ef1a0e6a9362da18eeea90755353083c963
wrampix <wrampix@gmail.com>
2011-06-25 21:17:06 +0200

    container activeness switch onstep 2

--- public/urls.js
+++ public/urls.js
@@ -1,52 +1,50 @@
+var urls = {};
 
-  var urls = {};
+urls.max = 10;
 
-  urls.max = 10;
+urls.more = function() {
+	var lastUrl = urls.form.getLast('p.url');
+	lastUrl.clone().inject(lastUrl, 'after').getFirst('input').set('value', 'http://').addEvents(urls.events).focus();
+};
 
-  urls.more = function(){
-    var lastUrl = urls.form.getLast('p.url');
-    lastUrl
-    .clone()
-    .inject(lastUrl,'after')
-    .getFirst('input')
-    .set('value','http://')
-    .addEvents(urls.events)
-    .focus();
-  };
+urls.less = function(_input) {
+	if (urls.form.getElements('p.url').length > 1) {
+		_input.getParent().dispose();
+		urls.form.getLast('p.url input').focus();
+	} else {
+		_input.highlight('#ddd')
+	}
+};
 
-  urls.less = function(_input) {
-    if(urls.form.getElements('p.url').length > 1) {
-      _input.getParent().dispose();
-      urls.form.getLast('p.url input').focus();
-    } else {
-      _input.highlight('#ddd')
-    }
-  };
+urls.events = {
+	keyup: function(ev) {
+		if (ev.key == 'enter' && ev.target.value.trim() != '') {
+			urls.more();
+		} else if (ev.key == 'backspace' && ev.target.value.trim() == '') {
+			urls.less(ev.target);
+		}
+	}
+};
 
-  urls.events = {
-    keyup : function(ev){
-      if(ev.key=='enter' && ev.target.value.trim()!='') {
-        urls.more();
-      } else if(ev.key=='backspace' && ev.target.value.trim()=='') {
-        urls.less(ev.target);
-      }
-    }
-  };
+urls.submit = function() {
+	urls.form.set('send', {
+		onSuccess: function(_res) {
+			$('content-1').getParent('div.content-floor').removeClass('content-floor-active');
+			$('content-2').set('html', _res).getParent('div.content-floor').addClass('content-floor-active');
+		}
+	});
+	urls.form.send();
+};
 
-  urls.submit = function(){
-    urls.form.set('send',{onSuccess:function(_res){$('content-2').set('html',_res)}});
-    urls.form.send();
-  };
+window.addEvent('domready', function() {
 
-  window.addEvent('domready',function(){
+	if (urls.form = $('content-1').getElement('form')) {
+		urls.form.getElements('p.url input').addEvents(urls.events);
+		$('done').addEvent('click', function(ev) {
+			ev.stop();
+			urls.submit();
+		})
+	}
 
-    if(urls.form = $('content-1').getElement('form')){
-      urls.form.getElements('p.url input').addEvents(urls.events);
-      $('done').addEvent('click',function(ev){
-        ev.stop();
-        urls.submit();
-       })
-    }
-
-  })
+})
 
